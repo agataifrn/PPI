e09a36aa7e9dd9b421cfdc76477e0033f9751cc3
Ágata <freire.agata@escolar.ifrn.edu.br>
2026-03-17 11:53:34 -0300

    primeira aula

---
+++ sala/códigos/ex1.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nome = 'ágata'; //o tipo da variável é inferido a partir do valor atribuído a ela, nesse caso, string
+let matri = 20231144010016;
+let outra; //define o tipo da variável, mas não atribui um valor a ela
+outra = true; //atribui um valor à variável
+console.log(nome);
+/* outra = 'agata'; //ERRADO, pois a variável foi definida como boolean e não pode receber uma string
+console.log(outra); */
+//# sourceMappingURL=ex1.js.map
---
+++ sala/códigos/ex1.ts
@@ -0,0 +1,13 @@
+let nome = 'ágata'; //o tipo da variável é inferido a partir do valor atribuído a ela, nesse caso, string
+let matri = 20231144010016;
+
+let outra: boolean //define o tipo da variável, mas não atribui um valor a ela
+outra = true; //atribui um valor à variável
+console.log(nome);
+
+let bim1: number | undefined = undefined; //a variável pode ser do tipo number ou undefined
+console.log(bim1);
+
+
+/* outra = 'agata'; //ERRADO, pois a variável foi definida como boolean e não pode receber uma string
+console.log(outra); */
